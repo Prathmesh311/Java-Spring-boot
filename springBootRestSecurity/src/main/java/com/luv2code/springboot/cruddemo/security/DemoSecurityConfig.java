10aec73b9a65d36ffd5247c415d2078b5dbacee5
Prathmesh311 <prathmesh.bhondave2@gmail.com>
2023-10-16 23:04:33 -0700

    JDBC Auth added

--- springBootRestSecurity/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ springBootRestSecurity/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.luv2code.springboot.cruddemo.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,12 +10,21 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
 	
 	@Bean
+	public UserDetailsManager userDetailsManager(DataSource dataSource) {
+		
+		return new JdbcUserDetailsManager(dataSource);
+		
+	}
+	
+	/*@Bean
 	public InMemoryUserDetailsManager userDetailsManager(){
 		
 		
@@ -38,7 +49,7 @@ public class DemoSecurityConfig {
 		
 		return new InMemoryUserDetailsManager(john, mary, susan);
 		
-	}
+	}*/
 	
 	@Bean
 	public SecurityFilterChain fileterChain(HttpSecurity http) throws Exception{
